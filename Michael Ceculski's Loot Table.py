23eef6b9a35aa4d159936c2074cf343683c17803
MichaelCeculski-Student <56096449+MichaelCeculski-Student@users.noreply.github.com>
2019-10-27 06:55:12 -0400

    Update Michael Ceculski's Loot Table.py

--- Michael Ceculski's Loot Table.py
+++ Michael Ceculski's Loot Table.py
@@ -8,6 +8,18 @@ loot_table = ["poisonous waste", "bat wing", "animal skin", "meat", "feather"]
 mob = mobs[random.randint(0,len(mobs)-1)]
 print("Fighting " + mob)
 
+#Combat phrase
+if mob == "goo manifestation":
+    print("So slimy... And slow.")
+elif mob == "bat":
+    print("Eeek! So scary! I'll smash 'em!")
+elif mob == "bear":
+    print("You being so big is unfair! But that won't stop me!")
+elif mob == "tiger":
+    print("You're too fast! But I'm still smarter!")
+else:
+    print("No way! I have to take the shot quickly!")
+
 # Kill the mob
 print("Killed " + mob)
 
@@ -15,20 +27,20 @@ print("Killed " + mob)
 if mob == "goo manifestation":
     print("Loot goo manifestation")
     print("Drop: poisonous waste")
-    print("Inventory: apple, energy elixir, wood, olive oil, poisonous waste")
+    print("Inventory: ", inventory + "poisonous waste")
 elif mob == "bat":
     print("Loot bat")
     print("Drop: bat wing")
-    print("Inventory: apple, energy elixir, wood, olive oil, bat wing")
+    print("Inventory: ", inventory + "bat wing")
 elif mob == "bear":
     print("Loot bear")
     print("Drops: animal skin, meat")
-    print("Inventory: apple, energy elixir, wood, olive oil, animal skin, meat")
+    print("Inventory: ", inventory + "animal skin, meat")
 elif mob == "tiger":
     print("Loot tiger")
     print("Drops: animal skin, meat")
-    print("Inventory: apple, energy elixir, wood, olive oil, animal skin, meat")
+    print("Inventory: ", inventory + "animal skin, meat")
 else:
     print("Loot eagle")
     print("Drop: feather")
-    print("Inventory: apple, energy elixir, wood, olive oil, feather")
+    print("Inventory: ", inventory + "feather")
