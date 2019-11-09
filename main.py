f32a7f9f9b9747bdb9d445968327558e2a520889
Shauna Gordon <shauna@shaunagordon.com>
2019-11-09 18:55:58 -0500

    Add main and mobs file for stubbing.

---
+++ main.py
@@ -0,0 +1,4 @@
+"""
+#   Main.py
+#   This will be the entrypoint of our game. Everything else can get pulled in to here as needed.
+"""
---
+++ mobs.py
@@ -0,0 +1,18 @@
+import random
+
+mobs = {
+    normal: {
+
+    },
+    rare: {
+
+    },
+    elite: {
+
+    },
+    named: {
+
+    }
+}
+
+def get_mob():
