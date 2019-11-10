30c9f0b4153317615ba639fea974eb74a08c61c0
Shauna Gordon <shauna@shaunagordon.com>
2019-11-09 20:12:15 -0500

    Add player creation and start of main

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-"""
-#   Main.py
-#   This will be the entrypoint of our game. Everything else can get pulled in to here as needed.
-"""
+'''
+This will be the entrypoint of our game. Everything else can get pulled in to here as needed.
+'''
+
+import player_template
+
+# Create a new player
+player = player_template.player_template
+player['name'] = input('Hello. What is your name? ')
+
+print(f"Welcome, {player['name']}")
